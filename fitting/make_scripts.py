deb4c68bb3833ddd61b22673a9df6804f6c4ca25
Sam Dixon <sam.dixon@berkeley.edu>
2019-04-10 14:19:12 -0700

    fix MW reddening and bug in csp LC fitting script, add mcmc fit flag

--- fitting/make_scripts.py
+++ fitting/make_scripts.py
@@ -19,23 +19,25 @@ TEMPLATE = """#!/bin/bash
 #$ -e /home/samdixon/host_unity/fitting/logs
 #$ -o /home/samdixon/host_unity/fitting/logs
 
-/home/samdixon/miniconda3/bin/python /home/samdixon/host_unity/fitting/mcmc_lc_{dataset}_fit.py {model} {start} {end} {err_float}"""
+/home/samdixon/anaconda3/bin/python /home/samdixon/host_unity/fitting/mcmc_lc_{dataset}_fit.py {model} {start} {end} {err_float} {no_mc}"""
 
 @click.command()
 @click.argument('dataset', type=click.Choice(['jla', 'csp', 'ps', 'all']))
 @click.option('-m', '--model', default='snemo7', type=click.Choice(['salt2', 'snemo2', 'snemo7']))
 @click.option('-e', '--err_floor', default=0., help='Desired error floor as fraction of maximum band flux.')
 @click.option('-n', '--njobs', default=16, help='Number of scripts to generate.')
-def cli(dataset, model, err_floor, njobs):
+@click.option('-c', '--no_mc', default=False, type=bool)
+def cli(dataset, model, err_floor, njobs, no_mc):
     if dataset == 'all':
         for err_floor in [0, 0.01, 0.02, 0.03]:
             for model in ['salt2', 'snemo2', 'snemo7']:
                 for dataset in ['jla', 'csp', 'ps']:
-                    make_scripts(dataset, model, err_floor, 16)
+                    for no_mc in [0, 1]:
+                        make_scripts(dataset, model, err_floor, 16, no_mc)
     else:
-        make_scripts(dataset, model, err_floor, 16)
+        make_scripts(dataset, model, err_floor, 16, no_mc)
 
-def make_scripts(dataset, model, err_floor, njobs):
+def make_scripts(dataset, model, err_floor, njobs, no_mc):
     if dataset == 'jla':
         n_sne = len(os.listdir('/home/samdixon/jla_light_curves/'))
     elif dataset == 'csp':
@@ -43,23 +45,28 @@ def make_scripts(dataset, model, err_floor, njobs):
     elif dataset == 'ps':
         data = pd.read_csv('/home/samdixon/foundation_photometry.txt', delimiter=', ', engine='python')
         n_sne = len(data.SN.unique())
-    submit_script_path = os.path.join(SCRIPT_DIR, 'submit_{}_{}_{:02d}.sh'.format(dataset, model, int(err_floor*100)))
+    if not no_mc:
+        submit_fname = 'submit_{}_{}_{:02d}_mcmc.sh'.format(dataset, model, int(err_floor*100))
+    else:
+        submit_fname = 'submit_{}_{}_{:02d}.sh'.format(dataset, model, int(err_floor*100))
+    submit_script_path = os.path.join(SCRIPT_DIR, submit_fname)
     with open(submit_script_path, 'w') as subf:
         for script_id in range(njobs):
             start = int(n_sne/njobs*script_id)
             end = int(n_sne/njobs*(script_id+1))
-            script_path = os.path.join(SCRIPT_DIR, '{}_{}_{:02d}_{}_{}.sh'.format(dataset,
-                                                                                  model,
-                                                                                  int(err_floor*100),
-                                                                                  start,
-                                                                                  end))
+            if not no_mc:
+                script_fname = '{}_{}_{:02d}_{}_{}_mcmc.sh'.format(dataset, model, int(err_floor*100), start, end)
+            else:
+                script_fname = '{}_{}_{:02d}_{}_{}.sh'.format(dataset, model, int(err_floor*100), start, end)
+            script_path = os.path.join(SCRIPT_DIR, script_fname)
             with open(script_path, 'w') as f:
                 f.write(TEMPLATE.format(dataset=dataset,
                                         model=model,
                                         err_int=int(err_floor*100),
                                         start=start,
                                         end=end,
-                                        err_float=err_floor))
+                                        err_float=err_floor,
+                                        no_mc=int(no_mc)))
             os.chmod(script_path, 0o755)
             subf.write('qsub {}\n'.format(os.path.abspath(script_path)))
     os.chmod(submit_script_path, 0o755)
--- fitting/mcmc_lc_jla_fit.py
+++ fitting/mcmc_lc_jla_fit.py
@@ -29,7 +29,7 @@ def modify_error(lc, error_floor=0.):
     return new_lc
     
     
-def fit_lc_and_save(lc, model_name, save_dir):
+def fit_lc_and_save(lc, model_name, save_dir, no_mc):
     name = lc.meta['SN']
     model = sncosmo.Model(source=model_name,
                           effects=[sncosmo.CCM89Dust()],
@@ -57,31 +57,38 @@ def fit_lc_and_save(lc, model_name, save_dir):
     model.set(z=z, t0=t0, mwebv=mwebv)
     phase_range = (-15, 45) if model_name=='salt2' else (-10, 40)
     wave_range = (3000, 7000) if model_name=='salt2' else None
+    save_path = os.path.join(save_dir, '{}.pkl'.format(name))
     try:
         minuit_result, minuit_fit_model = sncosmo.fit_lc(lc, model, model.param_names[:-2], bounds=bounds,
                                                          phase_range=phase_range, wave_range=wave_range,
                                                          warn=False, modelcov=modelcov)
-#         emcee_result, emcee_fit_model = sncosmo.mcmc_lc(sncosmo.select_data(lc, minuit_result['data_mask']),
-#                                                         minuit_fit_model,
-#                                                         model.param_names,
-#                                                         guess_t0=False,
-#                                                         bounds=bounds,
-#                                                         warn=False,
-#                                                         nwalkers=20,
-#                                                         modelcov=modelcov)
-        save_path = os.path.join(save_dir, '{}.pkl'.format(name))
-        pickle.dump(minuit_result, open(save_path, 'wb'))
+        if not no_mc:
+            emcee_result, emcee_fit_model = sncosmo.mcmc_lc(sncosmo.select_data(lc, minuit_result['data_mask']),
+                                                            minuit_fit_model,
+                                                            model.param_names,
+                                                            guess_t0=False,
+                                                            bounds=bounds,
+                                                            warn=False,
+                                                            nwalkers=20,
+                                                            modelcov=modelcov)
+            pickle.dump(emcee_result, open(save_path, 'wb'))
+        else:
+            pickle.dump(minuit_result, open(save_path, 'wb'))
     except:
         print('Fit to {} failed'.format(name))
         sys.stdout.flush()
     
     
 def main():
-    model_name, start, finish, err_floor = sys.argv[1:]
+    model_name, start, finish, err_floor, no_mc = sys.argv[1:]
     start = int(start)
     finish = int(finish)
     err_floor = float(err_floor)
-    save_dir = '/home/samdixon/host_unity/fitting/results_mw_reddening/jla_{}_{:02d}'.format(model_name, int(err_floor*100))
+    no_mc = bool(int(no_mc))
+    if no_mc:
+        save_dir = '/home/samdixon/host_unity/fitting/results_mw_reddening/jla_{}_{:02d}'.format(model_name, int(err_floor*100))
+    else:
+        save_dir = '/home/samdixon/host_unity/fitting/results_mw_reddening_mcmc/jla_{}_{:02d}'.format(model_name, int(err_floor*100))
     
     if not os.path.isdir(save_dir):
         os.makedirs(save_dir)
@@ -102,7 +109,7 @@ def main():
             except IOError:
                 print('Fitting {}'.format(name))
                 sys.stdout.flush()
-                fit_lc_and_save(lc, model_name, save_dir)
+                fit_lc_and_save(lc, model_name, save_dir, no_mc)
         else:
             continue
                 
